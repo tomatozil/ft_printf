b7d41d600996875a594f8edeae3d474b3d44ef88
jiyun <jiyun@student.42seoul.kr>
2022-07-21 20:01:21 +0900

    printf

--- ft_printf.h
+++ ft_printf.h
@@ -10,11 +10,7 @@
 
 void    print_char(char c, int *print_byte);
 void    print_str(char *s, int *print_byte);
-char    *check_base(char type);
-void    print_nbr_base(unsigned long nbr, char *base, int notation, int *print_byte);
 void    print_nbr(char type, unsigned long nbr, int *print_type);
-void    convert_to(char type, va_list arg_ptr, int *print_byte);
-int     is_valid_format(const char *format);
 int     ft_printf(const char *format, ... );
 
 #endif
